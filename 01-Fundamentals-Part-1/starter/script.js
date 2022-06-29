06825b9bac895da3d76a16a7aea30667218dd61a
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-06-28 20:37:01 -0500

    finished first quiz

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+let js = "amazing";
+if (js === "amazing") alert("Ok its working");
+console.log(40 + 8 + 23 - 10);
+
+const country = "USA";
+const continent = "North America";
+let population = 3330000;
+console.log(country, continent, population);
+
+const isIsland = 0;
+let language = "English";
+console.log(isIsland, population, country, language);
+
+console.log("Population of 3290000 splint in half is " + population/2);
+console.log(population + 1);
+
+function lessThanFinland(population) {
+    if (population < 5531000) {
+        return console.log("less populated than Finland")
+    }
+}
+lessThanFinland(population);
+
+function lessThanAverage(population) {
+    if (population < 3300000) {
+        return console.log("less populated than average")
+    } else {
+        return console.log("more populated than average")
+    }
+}
+lessThanAverage(population);
+
+let description = `Portugal is in Europe, 
+and its 11 million people speak portuguese`
+
+console.log(description)
